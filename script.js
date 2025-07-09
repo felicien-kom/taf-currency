fd1f08b409e362457084a660fcd5df4d0fd2f549
Felicien <felicienkom@gmail.com>
2025-07-09 16:50:10 +0100

    Improvement in Design and Responsive

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+// Recuperation des elements du HTML à manipuler
+const amount = document.getElementById("amount");
+const result = document.getElementById("result");
+const depart = document.getElementById("from");
+const arrivee = document.getElementById("to");
+const symbolAmount = document.getElementById("symbol-amount");
+const symbolResult = document.getElementById("symbol-result");
+const codeFrom = document.getElementById("code-from");
+const codeTo = document.getElementById("code-to");
+const convertBase = document.getElementById("convert-base");
+const dateChecked = document.getElementById("date-checked");
+const timeChecked = document.getElementById("time-checked");
+
+// Taux de change
+let taux = 0;
+
+// Fonction permettant d'obtenir le taux de change souhaite
 async function obtenirTauxDeChange(from, to) {
+    // Clé du taux de change pour le cache
     const CACHE_KEY = 'taux_change_' + from + '_' + to;
-    const UNE_HEURE = 30 * 1000; // en millisecondes
+    const VALIDITY_PERIOD = 60 * 1000; // en millisecondes
 
     // Verification du cache
     const cache = localStorage.getItem(CACHE_KEY);
-    if (cache) {
+    if (cache) {// Si le taux cherche se trouve dans le cache
         const { timestamp, taux } = JSON.parse(cache);
         const maintenant = Date.now();
-
-        if (maintenant - timestamp < UNE_HEURE) {
+        // et que de plus il est encore valide, ...
+        if (maintenant - timestamp < VALIDITY_PERIOD) {
             console.log('Taux chargé depuis le cache');
-            return taux;
+            // ... alors on recupere le taux depuis ce cache
+            return [taux, timestamp];
         }
     }
 
@@ -18,55 +37,66 @@ async function obtenirTauxDeChange(from, to) {
     try {
         const response = await fetch('https://api.exchangerate-api.com/v4/latest/' + from);
         if (!response.ok) {
-            throw new Error(`Erreur HTTP: ${response.status}`);
+            throw new Error(`Erreur HTTP lors de l'obtention du taux : ${response.status}`);
         }
 
         const data = await response.json();
 
-        var rate = data.rates[to];
+        let rate = data.rates[to];
         // console.log("Obtenir Taux");
-        if (rate === undefined) {
+        if (rate === undefined || rate === null) {
             // throw new Error('Le taux pour '+ from + ' --> ' + to +' n’a pas été trouvé.');
             rate = -1;
-            return rate;
+            return [rate, null];
         }
 
         // Stocker en cache avec timestamp vu que le rate est valide
+        let newTimestamp = Date.now();
         localStorage.setItem(CACHE_KEY, JSON.stringify({
-            timestamp: Date.now(),
+            timestamp: newTimestamp,
             taux : rate
         }));
 
         console.log('Taux mis en cache après appel API');
-        return rate;
+        return [rate, newTimestamp];
     } catch (error) {
         console.error('Erreur lors de la récupération du taux de change:', error);
-        return null;
+        return [null, null];
     }
 }
 
-var taux = 0;
-
-amount = document.getElementById("amount");
-result = document.getElementById("result");
-depart = document.getElementById("from");
-arrivee = document.getElementById("to");
-symbolAmount = document.getElementById("symbol-amount");
-symbolResult = document.getElementById("symbol-result");
-
+// Fonction utile pour actualiser les symboles en fonction des devises
 function getSymbol(curr){
     if(curr == 'EUR'){return "€";}
     else if(curr == 'USD'){return "$";}
-    else if(curr == 'XAF'){return "XAF";}
+    else if(curr == 'XAF'){return "X";}
     else if(curr == 'JPY'){return "¥";}
     else {return "X";}
 }
 
+// Fonction utile pour formater les Dates
+function presentDateChecked(date){
+    let y = date.getFullYear();
+    let m = date.getMonth() + 1;
+    let d = date.getDate();
+    return y + '-' + ((m < 10) ? '0' : '') + m + '-' + ((d < 10) ? '0' : '') + d;
+}
+// Fonction utile pour formater les Heures
+function presentTimeChecked(date){
+    let th = date.getHours();
+    let tm = date.getMinutes();
+    return '' + ((th < 10) ? '0' : '') + th + '-' + ((tm < 10) ? '0' : '') + tm;
+}
+
+// Fonction pour actualiser le taux et le resultat affiché à chaque modification du montant
 async function actualiser(){
-    f = document.getElementById("from").value;
-    t = document.getElementById("to").value;
+    let f = document.getElementById("from").value;
+    let t = document.getElementById("to").value;
     console.log(f + '-' + t);
-    taux = await obtenirTauxDeChange(f, t);
+    let tabTaux = await obtenirTauxDeChange(f, t);
+    taux = tabTaux[0];
+    let taxRateDate = new Date(tabTaux[1]);
+    // console.log(taxRateDate);
     /*
     obtenirTauxDeChange(f, t).then(rate => {
         if (rate !== null) {
@@ -76,35 +106,55 @@ async function actualiser(){
     });
     */
     // console.log("Suite !");
-    if(taux != -1){
-        if(amount.value == ''){
-            result.innerHTML = '';
+    if(taux === null){
+        result.value = 'Error !';
+        convertBase.innerHTML = '???';
+        dateChecked.innerHTML = 'YYYY-MM-DD';
+        timeChecked.innerHTML = 'HH-MM';
+    }
+    else if(taux != -1){// Si le taux est valide
+        convertBase.innerHTML = '' + taux;
+        if(amount.value == ''){// Si on a rien saisi,
+            result.value = '';// on ne fait rien
             // console.log("Vide ????" + amount.value);
         }
         else{
             console.log(" ***Taux*** " + taux);
-            result.innerHTML = '' + (taux * parseFloat(amount.value)).toFixed(3);
+            result.value = '' + (taux * parseFloat(amount.value)).toFixed(3);
             // console.log("Finish !");
         }
+        dateChecked.innerHTML = presentDateChecked(taxRateDate);
+        timeChecked.innerHTML = presentTimeChecked(taxRateDate);
+    }
+    else{
+        convertBase.innerHTML = '' + taux;
+        result.value = 'Enter a positive number';
+        dateChecked.innerHTML = 'YYYY-MM-DD';
+        timeChecked.innerHTML = 'HH-MM';
     }
 }
 
+// Dès le départ, on actualise pour commencer
+actualiser();
+
+// Modification du montant initial
 amount.addEventListener('input', ()=>{
     actualiser();
 });
 
+// Modification de la devise initiale
 depart.addEventListener('change', ()=>{
-    console.log(document.getElementById("from").value);
+    // console.log(document.getElementById("from").value);
     //alert("Hey !");
     actualiser();
+    codeFrom.innerHTML = depart.value;
     symbolAmount.innerHTML = getSymbol(depart.value);
 });
 
+// Modification de la devise finale
 arrivee.addEventListener('change', ()=>{
     //alert("Hey !");
     actualiser();
+    codeTo.innerHTML = arrivee.value;
     symbolResult.innerHTML = getSymbol(arrivee.value);
 });
-
-/*
-*/
